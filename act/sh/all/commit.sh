6a1eacc7155999f6cdae6805856e5f89419c6e0f
Shinji Hoshino <funny.pig.run@gmail.com>
2024-11-14 07:16:45 +0900

    Try to fix mistake in commit again.

--- act/sh/all/commit.sh
+++ act/sh/all/commit.sh
@@ -6,11 +6,11 @@ set -euox pipefail
 dir=$1
 # end metadata
 
-changes=$(git status --porcelain=v1 2>/dev/null | wc -l)
+changes=$(git -C "$dir" status --porcelain=v1 2>/dev/null | wc -l)
 if ((changes == 0)); then
     echo "There is nothing to commit."
 else
-    (cd "$dir" && git add .)
+    git -C "$dir" add "$dir"
     git -C "$dir" commit -m "upd."
     mapfile -t a < <(git -C "$dir" remote -v | awk '{ print $1 }' | sort | uniq)
     for r in "${a[@]}"; do
