29fcf15c1bed2dc18dc4c16addc29b0f0180a14a
Shinji Hoshino <funny.pig.run@gmail.com>
2024-11-18 22:12:50 +0900

    Added cross-compile script.

--- install.sh
+++ install.sh
@@ -3,3 +3,6 @@ set -euxo pipefail
 dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 cargo install --path "$dir"
+
+(cd "$dir" &&
+    CC="/opt/android-ndk/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android35-clang" AR="/opt/android-ndk/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-ar" cargo build --target aarch64-linux-android --release --config 'target.aarch64-linux-android.linker = "/opt/android-ndk/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android35-clang"')
